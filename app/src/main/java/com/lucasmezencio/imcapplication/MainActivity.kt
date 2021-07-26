1de4f895599dcb161829d6a8513c462178fa86ac
Lucas Mezencio <lucasmezencio@outlook.com>
2021-07-26 16:07:23 -0300

    create activity lifecycle logs

--- app/src/main/java/com/lucasmezencio/imcapplication/MainActivity.kt
+++ app/src/main/java/com/lucasmezencio/imcapplication/MainActivity.kt
@@ -2,10 +2,42 @@ package com.lucasmezencio.imcapplication
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        Log.w("lifecycle", "onCreate")
+    }
+
+    override fun onStart() {
+        super.onStart()
+        Log.w("lifecycle", "onStart")
+    }
+
+    override fun onResume() {
+        super.onResume()
+        Log.w("lifecycle", "onResume")
+    }
+
+    override fun onPause() {
+        super.onPause()
+        Log.w("lifecycle", "onPause")
+    }
+
+    override fun onStop() {
+        super.onStop()
+        Log.w("lifecycle", "onStop")
+    }
+
+    override fun onRestart() {
+        super.onRestart()
+        Log.w("lifecycle", "onRestart")
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        Log.w("lifecycle", "onDestroy")
     }
 }
